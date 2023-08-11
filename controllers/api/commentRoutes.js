2adb346f3817d78eaad838b4fd22ea44d5769225
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-11 18:12:08 -0500

    Set up necessary post and comment routes to create, update, and delete posts and comments when logged in

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,35 +1,57 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+// CREATE a new comment when a user is logged in
+router.comment('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newComment);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+// UPDATE a comment by its 'id' value when the user is logged in
+router.put('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    await Comment.update(req.body, {
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        user_id: req.session.user_id
+      }
+    })
+    const commentData = await Comment.findByPk(req.params.id);
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment with this id!' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE a comment when the user is logged in
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
